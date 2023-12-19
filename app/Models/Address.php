9a6d4d28f6c6fcf84a67884aed151062d00c4847
inakipalacios <inaki.palacios.m@gmail.com>
2023-12-19 18:13:53 +0100

    one to one relationship

---
+++ app/Models/Address.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+//1-1
+use App\Models\User;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'municipality',
+        'street',
+        'number'
+    ];
+
+    //1-1
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
