bb8ecd475dd9a2f9793c46a55d6952ff26657541
inakipalacios <inaki.palacios.m@gmail.com>
2024-01-11 09:33:47 +0100

    api ejemplo ruta

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Ejemplo ruta api
+ */
+/*Route::get('articles/{id}', function ($id){
+    return Article::find($id);
+});*/
